8b2dfc859a129011ed87349879ebb0337abec451
Emmanuelfwd <eruizhurtado25fwd@gmail.com>
2025-11-20 02:13:21 -0600

    Test basico primeras paginas (armazon)

---
+++ frontend/src/Pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = () => {
+    return (
+        <div className="container mt-5">
+            <h1>Página Principal</h1>
+            <p>Bienvenido a la app financiera.</p>
+        </div>
+    );
+};
+
+export default LandingPage;
---
+++ frontend/src/Pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+    return (
+        <div className="container mt-5">
+            <h1>Página de Registro</h1>
+        </div>
+    );
+};
+
+export default Register;
